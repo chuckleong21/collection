8e85a226413a32ed47033ce88dcf54891594f033
Chuck <chuckleong21@gmail.com>
2024-10-16 08:51:37 +0800

    [add] API class unit test

---
+++ tests/testthat/test-api.R
@@ -0,0 +1,19 @@
+box::use(
+  app/logic/api[...]
+)
+
+test_that("api class", {
+  u <- "https://bangumi.tv/subject/321885"
+  u2 <- "https://movie.douban.com/subject/3792799/"
+  u3 <- "https://www.douban.com/game/11639331/"
+  expect_s3_class(api(u), "api")
+  expect_s3_class(api(u), "bangumi")
+  expect_s3_class(api(u2), "douban")
+  expect_s3_class(api(u3), "douban")
+  expect_failure(expect_s3_class(api(u), c("douban")))
+  expect_error(api(3218885))
+  expect_error(api(3218885, domain = "domain"))
+  expect_error(api(3218885, domain = "douban"), "empty schema")
+  expect_error(api(3218885, domain = "douban", schema = "schema"), "Out-of-scope schema")
+  expect_s3_class(api(3218885, domain = "douban", schema = "movie"), "api")
+})
