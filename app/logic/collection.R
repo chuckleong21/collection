15b984ac80f4192465020c0baa33fa0fb4ce2043
chuckleong21 <chuckleong21@gmail.com>
2024-10-19 04:01:53 +0800

    [fix] diff logic & [add] merge method

--- app/logic/collection.R
+++ app/logic/collection.R
@@ -3,9 +3,10 @@ box::use(
   purrr[imap, map, set_names, map_vec, map2, walk2, list_rbind],
   duckdb[duckdb], 
   DBI[dbConnect, dbDisconnect],
-  dplyr[tbl, collect, select, filter, case_when, mutate, setequal],
+  dplyr[tbl, collect, select, filter, case_when, mutate, setequal, 
+        arrange, rows_upsert, row_number],
   waldo[compare],
-  assertthat[assert_that]
+  assertthat[assert_that, `on_failure<-`]
 )
 
 box::use(
@@ -35,7 +36,7 @@ local(envir = e, {
       })
     } else {
       x_desc <- map(x$data, \(i) {
-        purrr::map(i, \(j) split(j, j$status) |> map_vec(\(d) nrow(d)))
+        map(i, \(j) split(j, j$status) |> map_vec(\(d) nrow(d)))
       })
     }
     if(length(x$source) == 1) {
@@ -95,17 +96,39 @@ diff.collection <- function(x, y, compare = TRUE) {
   stopifnot('sources in "y" is not unique' = length(y$source) == 1)
   stopifnot('source in "x" is not from "database"' = x$source == "database")
   stopifnot('source in "y" is not from "import"' = y$source == "import")
+  diff_switch <- function(x, y) {
+    i <- nrow(x); j <- nrow(y)
+    if(i == 0) {
+      diff <- "behind"
+    } else {
+      if(j > 0) {
+        if(x$status == y$status & x$created_at == y$created_at) {
+          diff <- "identical"
+        } else {
+          if(y$status > x$status) {
+            diff <- "behind"
+          }
+          if(y$status == x$status) {
+            if(x$created_at > y$created_at) {
+              diff <- "ahead"
+            } else {
+              diff <- "behind"
+            }
+          }
+        }
+      } else {
+        diff <- "ahead"
+      }
+    }
+    return(diff)
+  }
   
   diff_sid <- function(x, y) {
-    args <- get("args", environment())
     sid <- unique(c(x$subject_id, y$subject_id))
     compare_sid <- map_vec(seq_along(sid), \(i) {
       u <- filter(x, subject_id == sid[i]) |> select(-id)
       v <- filter(y, subject_id == sid[i]) |> select(-id)
-      case_when(nrow(u) != 0 & nrow(v) == 0 ~ "behind", 
-                nrow(u) != 0 & nrow(v) != 0 & !setequal(u, v) ~ "behind",
-                nrow(u) == 0 & nrow(v) != 0 ~ "ahead", 
-                .default = "identical") 
+      diff_switch(x = u, y = v)
     })
     branch <- map(c("behind", "ahead"), \(k) {
       filter(y, subject_id %in% sid[which(compare_sid == k)]) |> 
@@ -129,7 +152,7 @@ local(envir = e, {
 
 collection_get <- function(source = c("database", "import"), 
                            file = NULL, dbdir = NULL) {
-  on.exit(dbDisconnect(con))
+  on.exit(dbDisconnect(con), add = TRUE)
   if(!all(source %in% c("import", "database"))) {
     stop('One or more elements in argument "source" is invalid')
   }
@@ -227,7 +250,7 @@ print.collection_diff <- function(x) {
   } else {
     print(x$diff)
   }
-
+  
 }
 
 # merge method
@@ -235,8 +258,31 @@ print.collection_diff <- function(x) {
 merge <- function(x, y, ...) {
   UseMethod("merge")
 }
-merge.collection <- function(x, y, diff) {
+merge.collection_diff <- function(diff, database = NULL) {
+  on.exit(message(sprintf("updated %g records", updates)), add = TRUE)
+  is_database <- function(x) {
+    assert_that(inherits(x, "collection"))
+    x$source == "database"
+  }
+  on_failure(is_database) <- function(call, env) {
+    paste0(deparse(call$x), " is not a database collection")
+  }
+  database <- database %||% collection("database")
+  assert_that(is_database(database))
   
+  out <- map2(branch(diff), database$data, \(x, y) {
+    rows_upsert(y, 
+                filter(x, diff == "behind") |> 
+                  select(-diff), 
+                by = "subject_id")
+  }) 
+  updates <- out |>
+    map_vec(~(length(which(is.na(.x$id))))) |> 
+    sum()
+  out <- out |>
+    map(~(arrange(.x, created_at) |> 
+            mutate(id = row_number())))
+  structure(list(source = "database", data = out), class = "collection")
 }
 merge.data.frame <- function(x, y, ...) {
   merge.data.frame(x, y, ...)
@@ -252,4 +298,8 @@ local(envir = e, {
 local(envir = e, {
   print.collection_diff
   .S3method("print", "collection_diff")
+})
+local(envir = e, {
+  merge.collection_diff 
+  .S3method("merge", "collection_diff")
 })
