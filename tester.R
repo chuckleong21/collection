68e6ae6281047996b7ac90a652f0f2c4776b3bac
Chuck <chuckleong21@gmail.com>
2024-10-20 17:46:06 +0800

    [edit] scripting file

--- tester.R
+++ tester.R
@@ -12,22 +12,21 @@ bangumi <- api(321885, domain = "bangumi")
 
 database <- collection("database")
 imported <- collection("import", "app/static/豆伴(58485907)_2.xlsx")
-d <- diff(database, imported)
-r <- request(douban_movie)
+r <- request(douban_book)
 h <- httr2::resp_body_html(r)
-res <- xpaths$douban$movie |> 
-  purrr::map(~fetch_douban_movie(h, .x)) |>
+  
+res <- xpaths$douban$book |> 
+  purrr::map(~fetch_douban_book(h, .x)) |>
   purrr::list_flatten(name_spec = "{inner}")
 res$id <- NA_integer_
-res$subject_id <- as.character(douban_movie$id)
-res$type <- douban_movie$schema
-res$status <- NA
-res$my_rating <- NA
+res$subject_id <- as.character(douban_book$id)
+res$type <- douban_book$schema
+res$status <- NA_character_
+res$my_rating <- NA_real_
 res$url <- r$url
 res$created_at <- lubridate::force_tz(lubridate::now(), "UTC")
 dplyr::as_tibble(res) |>
-  dplyr::select(id, subject_id, type, title, cover, year, region,
-                genre, director, starring, status, rating, my_rating,
-                url, created_at)
+  dplyr::select(id, subject_id, type, title, cover, year, author,
+                publisher, status, rating, my_rating,url, created_at)
 
 # fetch_douban_movie
