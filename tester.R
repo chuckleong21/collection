74e6f8dde3c79fea0312d7b8a502199f0270e16c
Chuck <chuckleong21@gmail.com>
2024-10-21 15:44:19 +0800

    [edit] scripting file

--- tester.R
+++ tester.R
@@ -5,16 +5,17 @@ box::use(
 )
 
 douban_movie <- api(35882838, domain = "douban", schema = "movie")
+douban_movie2 <- api(36152618, domain = "douban", schema = "movie")
+fetch(douban_movie)
+fetch(douban_movie2)
 douban_book <- api(36879010, domain = "douban", schema = "book")
 douban_music <- api(35393951, domain = "douban", schema = "music")
 douban_game <- api(11639331, domain = "douban", schema = "game")
-bangumi <- api(321885, domain = "bangumi")
-
-fetch(douban_movie)
 fetch(douban_book)
-fetch(douban_music)
 fetch(douban_game)
+fetch(douban_music)
 
+bangumi <- api(321885, domain = "bangumi")
 database <- collection("database")
 imported <- collection("import", "app/static/豆伴(58485907)_2.xlsx")
 
