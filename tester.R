7658fb6703c4afbc99df0f54d7f775a7b8c0fdbc
Chuck <chuckleong21@gmail.com>
2024-10-21 10:58:34 +0800

    [update] scripting file

--- tester.R
+++ tester.R
@@ -10,23 +10,13 @@ douban_music <- api(35393951, domain = "douban", schema = "music")
 douban_game <- api(11639331, domain = "douban", schema = "game")
 bangumi <- api(321885, domain = "bangumi")
 
+fetch(douban_movie)
+fetch(douban_book)
+fetch(douban_music)
+fetch(douban_game)
+
 database <- collection("database")
 imported <- collection("import", "app/static/豆伴(58485907)_2.xlsx")
-r <- request(douban_book)
-h <- httr2::resp_body_html(r)
-  
-res <- xpaths$douban$book |> 
-  purrr::map(~fetch_douban_book(h, .x)) |>
-  purrr::list_flatten(name_spec = "{inner}")
-res$id <- NA_integer_
-res$subject_id <- as.character(douban_book$id)
-res$type <- douban_book$schema
-res$status <- NA_character_
-res$my_rating <- NA_real_
-res$url <- r$url
-res$created_at <- lubridate::force_tz(lubridate::now(), "UTC")
-dplyr::as_tibble(res) |>
-  dplyr::select(id, subject_id, type, title, cover, year, author,
-                publisher, status, rating, my_rating,url, created_at)
+
 
 # fetch_douban_movie
