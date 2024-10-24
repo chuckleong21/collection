19a2e93795a10da06b3b9c9d51c722466fe7cc03
chuckleong21 <chuckleong21@gmail.com>
2024-10-24 16:51:14 +0800

    [add] scripting file

--- tester.R
+++ tester.R
@@ -1,14 +1,14 @@
 box::use(
   app/logic/api[...], 
+  app/logic/request[...], 
+  app/logic/fetch[...], 
+  app/logic/fill[fill],
   app/logic/collection[...],
   app/logic/api_helpers[...],
 )
 
-bangumi <- api(321885, domain = "bangumi")
+database <- collection("database")
+fill(database)
 
-database <- collection("database", dbdir = "tests/testthat/database.duckdb")
-imported <- collection("import", "app/static/豆伴(58485907)_2.xlsx")
-d <- diff(database, imported)
-merge(d, database)
 
-# fetch_douban_movie
+
